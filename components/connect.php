02ce5011c8da0f0a828473f14a369b7a5ef3c45b
andrey100f <andreiforminte19@yahoo.com>
2024-03-30 15:50:54 +0200

    Implemented the connection to database.

---
+++ components/connect.php
@@ -0,0 +1,26 @@
+<?php
+    $db_name = "mysql:host=localhost;dbname=review_system";
+    $db_user_name = "root";
+    $db_password = "@Gabitzuf191002@";
+
+    $connection = new PDO($db_name, $db_user_name, $db_user_password);
+
+    function create_unique_id() {
+        $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $characters_length = strlen($characters);
+        $random_string = "";
+        
+        for($i = 0; $i < 20; $i++) {
+            $random_string .= $characters[mt_rand(0, $characters_length - 1)];
+        }
+
+        return $random_string;
+    }
+
+    if(isset($_COOKIE["user_id"])) {
+        $user_id = $_COOKIE["user_id"];
+    }
+    else {
+        $user_id = "";
+    }
+?>
