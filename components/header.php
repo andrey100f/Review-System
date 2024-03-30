779d0fef1357c78494355293e5a8215143c0abf8
andrey100f <andreiforminte19@yahoo.com>
2024-03-30 17:01:08 +0200

    Added the header.

---
+++ components/header.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <section class="flex">
+        <a href="all_posts.php" class="logo">Review System</a>
+
+        <nav class="navbar">
+            <a href="all_posts.php" class="fa-regular fa-eye"></a>
+            <a href="login.php" class="fa-solid fa-right-to-bracket"></a>
+            <a href="register.php" class="fa-regular fa-registered"></a>
+        </nav>
+    </section>
+</header>
